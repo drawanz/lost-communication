f152c24613abd7b5576ceb2e62608668f7151ac4
Leonidas <leonidas.drawanz@gmail.com>
2022-09-02 09:36:38 -0300

    feat: adicionando schemas iniciais

---
+++ app/server/models/lost.py
@@ -0,0 +1,38 @@
+from datetime import date
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, Field
+
+
+class LostSchema(BaseModel):
+    nome: str = Field(...)
+    email: EmailStr = Field(...)
+    CPF: int = Field(...)
+    latitude: float = Field(...)
+    longitude: float = Field(...)
+    lavoura: str = Field(...)
+    data_colheita: date = Field(...)
+    evento: str = Field(...)
+
+
+class UpdateStudentModel(BaseModel):
+    nome: str = Field(...)
+    email: EmailStr = Field(...)
+    CPF: int = Field(...)
+    latitude: float = Field(...)
+    longitude: float = Field(...)
+    lavoura: str = Field(...)
+    data_colheita: date = Field(...)
+    evento: str = Field(...)
+
+
+def ResponseModel(data, message):
+    return {
+        "data": [data],
+        "code": 200,
+        "message": message,
+    }
+
+
+def ErrorResponseModel(error, code, message):
+    return {"error": error, "code": code, "message": message}
