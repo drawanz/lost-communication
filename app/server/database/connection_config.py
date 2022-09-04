c92d64248f2fdb584fe6c99cef12292272205dea
Leonidas <leonidas.drawanz@gmail.com>
2022-09-04 08:50:24 -0300

    adicionando um valor default para a str de conexão

--- app/server/database/connection_config.py
+++ app/server/database/connection_config.py
@@ -4,7 +4,7 @@ from decouple import config
 
 conn_str = config("MONGO_DETAILS")
 
-client = motor.motor_asyncio.AsyncIOMotorClient(conn_str, serverSelectionTimeoutMS=5000)
+client = motor.motor_asyncio.AsyncIOMotorClient(conn_str or 'test', serverSelectionTimeoutMS=5000)
 
 database = client.lost
 
