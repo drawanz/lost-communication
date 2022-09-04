b65561986d1da6e70941989f6fe3be378f6f140c
Leonidas <leonidas.drawanz@gmail.com>
2022-09-04 09:01:39 -0300

    usando o module OS para import de env var

--- app/server/database/connection_config.py
+++ app/server/database/connection_config.py
@@ -1,10 +1,13 @@
 import motor.motor_asyncio
-from decouple import config
+# from decouple import config
+import os
 
 
-conn_str = config("MONGO_DETAILS")
+# conn_str = config("MONGO_DETAILS")
+conn_str = os.environ.get("MONGO_DETAILS", "mongodb")
 
-client = motor.motor_asyncio.AsyncIOMotorClient(conn_str or 'test', serverSelectionTimeoutMS=5000)
+
+client = motor.motor_asyncio.AsyncIOMotorClient(conn_str, serverSelectionTimeoutMS=5000)
 
 database = client.lost
 
