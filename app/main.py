633ee346125d2d51387501cc0755dadcd8f2c843
Leonidas <leonidas.drawanz@gmail.com>
2022-09-05 10:53:59 -0300

    feat: adicionando a porta como env var

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 import uvicorn
+import os
+
+PORT = os.environ.get("PORT", "8000")
 
 if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server.app:app", host="0.0.0.0", port=PORT, reload=True)
