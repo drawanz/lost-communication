fb5dea0b3a42a37879a098de5434e73cce86e5d4
Leonidas <leonidas.drawanz@gmail.com>
2022-09-05 11:32:35 -0300

    chore: tentando resolver o bug de não estar int a env

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 import os
 
-port_from_env = os.environ.get("PORT", 8000)
+port_from_env = os.environ.get("PORT")
 
 if __name__ == "__main__":
     uvicorn.run("server.app:app", host="0.0.0.0", port=int(port_from_env), reload=True)
