a801269cf57efdc85a39163cb9ba387b32914571
Leonidas <leonidas.drawanz@gmail.com>
2022-09-05 11:15:47 -0300

    fix: resolvido o bug de int

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 import os
 
-PORT = os.environ.get("PORT", "8000")
+port_from_env = os.environ.get("PORT", "8000")
 
 if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="0.0.0.0", port=PORT, reload=True)
+    uvicorn.run("server.app:app", host="0.0.0.0", port=int(port_from_env), reload=True)
